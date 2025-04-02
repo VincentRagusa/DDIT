26e90405d86bce0132d8b859b067aedd67e55bd5
Vincent Ragusa <vincent.ragusa.94@gmail.com>
2025-04-02 16:01:42 -0400

    added support for permutation p-values. Introduced a temporary dataframe concept where the p-value function can temporarily redirect all essential functions to work with an alternative dataframe. This was a simpler alternative to passing the dataframe as a parameter or adding and removing extra columns from the main dataframe.

--- rewrite.py
+++ rewrite.py
@@ -26,6 +26,8 @@ class DDIT:
     def __init__(self, verbose:bool=False) -> None:
         self.verbose:bool = verbose
         self.df:pl.DataFrame = None
+        self._temp_df:pl.DataFrame = None
+        self._use_temp_df = False
 
 
     def read_csv(self,path_to_csv:str) -> None:
@@ -77,6 +79,10 @@ class DDIT:
             print(f"Computing the entropy of {column_name}")
         alias = f"H({column_name})"
         select = pl.col(column_name) .cast(pl.Utf8) .unique_counts() .entropy(base=2) .alias(alias)
+
+        if self._use_temp_df:
+            return self._temp_df.select(select)
+
         return self.df.select(select)
 
 
@@ -90,9 +96,14 @@ class DDIT:
         column_names.sort()
         if self.verbose:
             print(f"Joining columns {column_names}")
+
         alias = "&".join(column_names)
         with_columns = pl.struct(column_names) .alias(alias)
-        self.df = self.df.with_columns(with_columns)
+
+        if self._use_temp_df:
+            self._temp_df = self._temp_df.with_columns(with_columns)
+        else:
+            self.df = self.df.with_columns(with_columns)
 
 
     def evaluate_expression(self,entropy_expression:str) -> pl.DataFrame:
@@ -147,7 +158,7 @@ class DDIT:
         if len(variables) > 1:
             # A&B
             sorted_expression = "&".join(sorted(variables))
-            if sorted_expression not in self.df:
+            if sorted_expression not in (self._temp_df if self._use_temp_df else self.df):
                 self.join(variables)
             return self.entropy(sorted_expression)
 
@@ -155,15 +166,83 @@ class DDIT:
         return self.entropy(variables[0])
 
 
+    def evaluate_expression_with_p_value(self,
+                                         entropy_expression:str,
+                                         seed:int=None,
+                                         num_iterations:int=100) -> pl.DataFrame:
+        """wraps DDIT.evaluate_expression() and also computes a p-value for the result.
+        The p-value is computed via the permutation method. See
+        https://en.wikipedia.org/wiki/Permutation_test for further information.
+
+        Args:
+            entropy_expression (str): An entropy expression in normal form.
+            Normal form is 'X: ... :Y|Z', where X, Y, and Z can be single or joint
+            random variables and there can be arbitrarily many 'shared' variables.
+            No single variable can appear more than once. Use joint variable aliases
+            to prevent multiple appearences of a variable if necessary. It is never
+            necessary.
+
+            seed (int, optional): Sets the seed of the random number generator
+            to allow for reproducable results. Defaults to None.
+
+            num_iterations (int, optional): The number of random permutations to test
+            before computing the p-value. Defaults to 100.
+
+        Returns:
+            pl.DataFrame: DataFrame containing the entropy of the given expression and its p-value.
+        """
+        true_result_df = self.evaluate_expression(entropy_expression)
+
+        split_expression = entropy_expression.split("|")
+        joint_variables = split_expression[0].split(":") + split_expression[1:]
+        required_variables = [var for joint in joint_variables for var in joint.split("&")]
+
+        test_results_df = pl.DataFrame()
+
+        self._use_temp_df = True
+
+        for i in range(num_iterations):
+            if seed is not None:
+                self._temp_df = self.df.select(pl.col(required_variables).shuffle(seed=seed+i))
+            else:
+                self._temp_df = self.df.select(pl.col(required_variables).shuffle())
+
+            temp_ee = self.evaluate_expression(entropy_expression)
+            test_results_df = pl.concat([test_results_df,temp_ee],how="vertical")
+
+        self._use_temp_df = False
+        self._temp_df = None
+
+        above_select = (
+            pl.col(f"H({entropy_expression})") >= true_result_df[f"H({entropy_expression})"]
+            ).sum().alias("right p-value")
+
+        below_select = (
+            pl.col(f"H({entropy_expression})") <= true_result_df[f"H({entropy_expression})"]
+            ).sum().alias("left p-value")
+
+        count_df = test_results_df.select([above_select,below_select])
+        p_value_df = count_df.select(pl.col("left p-value","right p-value") / num_iterations)
+
+        return pl.concat([true_result_df,p_value_df],how="horizontal")
+
+
 if __name__ == "__main__":
-    ddit = DDIT(verbose=False)
-    # ddit.read_csv("./xor_data.csv")
+    ddit = DDIT(verbose=True)
 
-    my_data = {"X":[0,0,1,1],
-               "Y":['a','b','a','b']}
+    ddit.read_csv("./xor_data.csv")
+
+    my_data = {"A":[0,0,1,1],
+               "B":['a','b','a','b'],
+               "C":["a",0,0,"a"]}
+
     ddit.add_column(my_data)
-    ddit.add_column({"Z":["a",0,0,"a"]})
 
-    ee = ddit.evaluate_expression("X:Y|Z")
+    ee = ddit.evaluate_expression("X:A:C&B|Y&Z")
     print(ee)
     print(ddit.df)
+
+    ddit.verbose = False
+
+    eep = ddit.evaluate_expression_with_p_value("X:A:C&B|Y&Z",num_iterations=200)
+    print(eep)
